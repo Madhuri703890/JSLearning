1db46ff560777ce9b0924c0454144951a1301014
Madhuri703890 <madhuri703890@gmail.com>
2023-09-20 06:44:38 -0700

    basic JS
    commit

---
+++ basic.js
@@ -0,0 +1,25 @@
+console.log(myFullName)
+var myFullName="madhuri yadav"
+console.log(myFullName)
+var myFullName="madhuri dadaso yadav"
+console.log(myFullName)
+var myLove
+myLove="I LOve only JS"
+console.log(myLove)
+var firstName,lastName,age,address,pinCode,gender,isMarried
+firstName="Madhuri"
+lastName="Yadav"
+age="30"
+address="pandharpur"
+piCode="413304"
+gender="female"
+iMarried="Yes"
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(gender)
+console.log(iMarried)
+
+
+
+
