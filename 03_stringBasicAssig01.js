be8ab2d7110c30f7fd9b491dda675b6ff516106c
Madhuri703890 <madhuri703890@gmail.com>
2023-09-24 23:58:28 -0700

    function bankDetails

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,13 @@
+function stringBasics() {
+    
+}
+console.log("==========================Q.1========================");
+console.log('My Dream company is "TCS"');
+console.log("==========================Q.2========================");
+var hobby1="writting";
+var hobby2="music";
+var hobby3="designning";
+console.log("MY hobbies are",hobby1, hobby2, hobby3);
+console.log("=============Sum of Total Characters=============");
+var sum=hobby1.length+hobby2.length+hobby3.length;
+console.log("Total chracters are=",sum);
