10d17ceced8556ed07f1588005dc072a8e998bd4
Madhuri703890 <madhuri703890@gmail.com>
2023-10-11 01:28:02 -0700

    JS date method

---
+++ 09_date.js
@@ -0,0 +1,14 @@
+let d=new Date();
+console.log(d);
+console.log(d.getDate());
+console.log(d.toDateString());
+console.log(d.toTimeString());
+
+var dt= new Date("23 June 1991");
+console.log(dt);
+
+var dd=new Date("2015-07-09");
+console.log(dd);
+
+var dm=new Date("June 18, 2019 23:30:00");
+console.log(dm);
